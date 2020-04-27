d8c595b3f9fa8228924e6aa0a31afe0287e3cc37
baba_s <shotababa1206@gmail.com>
2020-04-27 19:53:04 +0900

    first commit

---
+++ Scripts/BatteryInfo.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace UniBatteryInfo
+{
+	/// <summary>
+	/// バッテリーの状態
+	/// </summary>
+	public enum BatteryStatus
+	{
+		/// <summary>
+		/// 不明
+		/// </summary>
+		UNKNOWN = 0,
+
+		/// <summary>
+		/// 充電中
+		/// </summary>
+		CHARGING = 1,
+
+		/// <summary>
+		/// 放電中
+		/// </summary>
+		DISCHARGING = 2,
+
+		/// <summary>
+		/// 充電していない
+		/// </summary>
+		NOT_CHARGING = 3,
+
+		/// <summary>
+		/// 満タン
+		/// </summary>
+		FULL = 4
+	}
+
+	/// <summary>
+	/// バッテリーの情報を管理するクラス
+	/// </summary>
+	public static class BatteryInfo
+	{
+		/// <summary>
+		/// バッテリーの残量（ 0.0 ～ 1.0 ）を返します
+		/// </summary>
+		public static float GetLevel()
+		{
+			return SystemInfo.batteryLevel;
+		}
+
+		/// <summary>
+		/// バッテリーの状態を返します
+		/// </summary>
+		public static BatteryStatus GetStatus()
+		{
+			return ( BatteryStatus ) SystemInfo.batteryStatus;
+		}
+	}
+}
